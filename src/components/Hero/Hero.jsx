c87b6f945922468fb87c1df3cfc1933ae23df898
Anamika1608 <anamikaagg18@gmail.com>
2024-07-13 10:27:32 +0530

    Update Hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,7 +9,7 @@ export function FlipWordsDemo() {
   return (
     <div className="text-center p-5 sm:p-8">
             <h1 className="text-4xl p-3 font-medium drop-shadow sm:text-5xl mt-16 ">Making tech <FlipWords words={words} /> for <br />
-            <h1 className="sm:pt-5 pb-5">underrepresented communities</h1></h1>
+            <h1 className="sm:pt-5 pb-3">underrepresented communities</h1></h1>
             {/* <button className="btn bg-[#f0f0f0] m-2 p-2 w-30 rounded-xl hover:bg-[#FFF0DA] text-medium sm:w-40 sm:p-2 sm:m-3 font-semibold">Get Started</button>
             <button className="btn bg-[#f0f0f0] m-2 p-2 w-30 rounded-xl hover:bg-[#FFF0DA] text-medium sm:w-40 sm:p-2 sm:m-3 font-semibold">Learn More</button> */}
             <MovingBorderDemo/>      
